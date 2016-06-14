acda3b0fbfe143b48f2dc23bf50556a8fccaa143
Steve Puddick <spuddick@liberal.ca>
2016-06-14 12:35:33 -0400

    project set up and file name changes

--- admin-code-editor.php
+++ admin-code-editor.php
@@ -10,18 +10,18 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Admin_Code_Editor
  *
  * @wordpress-plugin
  * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin URI:        http://example.com/admin-code-editor-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Your Name or Your Company
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       admin-code-editor
  * Domain Path:       /languages
  */
 
@@ -32,30 +32,30 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
+ * This action is documented in includes/class-admin-code-editor-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_admin_code_editor() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-admin-code-editor-activator.php';
+	Admin_Code_Editor_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
+ * This action is documented in includes/class-admin-code-editor-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_admin_code_editor() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-admin-code-editor-deactivator.php';
+	Admin_Code_Editor_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook( __FILE__, 'activate_admin_code_editor' );
+register_deactivation_hook( __FILE__, 'deactivate_admin_code_editor' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-admin-code-editor.php';
 
 /**
  * Begins execution of the plugin.
@@ -66,10 +66,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_admin_code_editor() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new Admin_Code_Editor();
 	$plugin->run();
 
 }
-run_plugin_name();
+run_admin_code_editor();
---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+var sass = require('gulp-sass');
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var merge = require('merge-stream');
+var outputDir = 'builds/development';
+
+
+// Define default destination folder
+var public_dest = 'public/';
+var admin_dest = 'admin/';
+
+gulp.task('copypublic', function() {
+   gulp.src('./bower_components/foundation-sites/dist/foundation.min.js')
+   .pipe(gulp.dest('./themes/liberal-master-2016/front-end-plugins/foundation'));
+
+   gulp.src('./bower_components/foundation-sites/dist/foundation.min.css')
+   .pipe(gulp.dest('./themes/liberal-master-2016/front-end-plugins/foundation'));
+
+   gulp.src('./bower_components/fastclick/lib/fastclick.js')
+   .pipe(gulp.dest('./themes/liberal-master-2016/front-end-plugins/fastclick'));
+
+}); 
+
+gulp.task('sass-master', function () {
+  gulp.src('themes/liberal-master-2016/scss/master.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('themes/liberal-master-2016/css'));
+});
+
+ 
+gulp.task('sass-liberal', function () {
+  gulp.src('themes/liberal-2016/scss/liberal-2016.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('themes/liberal-2016/css'));
+});
+
+gulp.task('watch', function() {
+  //gulp.watch('**/js/*.js', ['js']);
+  gulp.watch('./themes/liberal-master-2016/scss/*.scss', ['sass-master']);
+  gulp.watch('./themes/liberal-2016/scss/*.scss', ['sass-liberal']);
+  //gulp.watch('./plugins/**/scss/*.scss', ['sass']);
+});
+
+
+gulp.task('default', ['copypublic', 'sass-master', 'sass-liberal', 'watch']);
+
+
+/**
+ *
+ * Tutorial from https://travismaynard.com/writing/getting-started-with-gulp
+ *
+ */
+
+
+/*
+
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+
+ */
