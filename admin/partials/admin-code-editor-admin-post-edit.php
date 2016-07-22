7299eddf951bba669b2f1fee1b8226ca0693d89c
Steve Puddick <spuddick@liberal.ca>
2016-07-22 15:31:59 -0400

    add: post edit interface components

--- admin/partials/admin-code-editor-admin-post-edit.php
+++ admin/partials/admin-code-editor-admin-post-edit.php
@@ -38,23 +38,73 @@
 	  <!-- Tab panes -->
 	  <div class="tab-content">
 	    <div role="tabpanel" class="tab-pane active" id="html-edit">
-	    	<div class="btn-group" role="group" aria-label="...">
-				  <button type="button" class="btn btn-primary "><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>Save</button>
+				<div class="" style="padding-top:10px;" >
+					
+					<div class="alert alert-warning" role="alert">
+					  <strong>Warning!</strong> Better check yourself, you're not looking too good.
+					</div>	    	
+		    	
+		    	<div class="clearfix" >
+					  <div class="" >
+						  <p class="text-muted" ><span class="glyphicon glyphicon glyphicon glyphicon-cog" aria-hidden="true"></span> Proprocessing with <strong>HAML</strong>. Positioned <strong>before post content</strong>. wpautop <strong>enabled</strong>. Display only on single template <strong>Disabled</strong>. <a href='#change-settings-modal' role="button" data-toggle="modal" data-backdrop="true" aria-expanded="false" aria-controls="change-preprocessor-modal">Change Settings</a></p>
+					  </div>
+
+					</div>
+					<div class="clearfix" style="background-color:#f5f5f5;padding:10px;" >
+					  <button type="button" class="btn btn-primary pull-right"><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Save</button>
+						<p class="pull-right text-success" ><span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span> Saved</p>
+					</div>
+
+
+					<div class="modal fade" tabindex="-1" role="dialog" id="change-settings-modal" >
+					  <div class="modal-dialog">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					        <h4 class="modal-title">HTML Settings</h4>
+					      </div>
+					      <div class="modal-body">
+	      					<div>
+										<h4>General</h4>
+										<label class="checkbox"><input type="checkbox" value="">Disable wpautop <span class="text-muted">(automatically wraps in paragraph tag)</span></label>
+										<label class="checkbox"><input type="checkbox" value="">Only display on single template</label>
+
+										<h4>Position</h4>
+										<label class="radio"><input type="radio" value="">before post content </label>
+										<label class="radio"><input type="radio" value="">after post content</label>
+										<label class="radio"><input type="radio" value="">do not display <span class="text-muted">(only the post content is displayed)</span></label>
+										<label class="radio"><input type="radio" value="">display exclusively <span class="text-muted">(post content will be hidden)</span></label>
+										<label class="radio"><input type="radio" value="">embed with shortcode [wp-ace-html] <span class="text-muted">(code will be inserted where shortcode is placed)</span></label>
+
+										<h4>Pre Processor</h4>
+										<label class="radio"><input type="radio" value="">Option 1</label>
+										<label class="radio"><input type="radio" value="">Option 2</label>
+										<label class="radio"><input type="radio" value="">Option 3</label>						
+									</div>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					      </div>
+					    </div><!-- /.modal-content -->
+					  </div><!-- /.modal-dialog -->
+					</div><!-- /.modal -->
+
 				</div>
-				<div class="alert alert-warning alert-dismissible" role="alert">
-				  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				  <strong>Warning!</strong> Better check yourself, you're not looking too good.
-				</div>
-				<div>
-					<label class="checkbox-inline"><input type="checkbox" value="">Option 1</label>
-					<label class="checkbox-inline"><input type="checkbox" value="">Option 2</label>
-					<label class="checkbox-inline"><input type="checkbox" value="">Option 3</label>
-				</div>
+
+
 	    </div>
 	    <div role="tabpanel" class="tab-pane" id="css-edit">
-				<div class="alert alert-danger" role="alert">
-				  <a href="#" class="alert-link">...</a>
-				</div>	    	
+		    	<div class="clearfix" >
+					  <div class="" >
+						  <p class="text-muted" ><span class="glyphicon glyphicon glyphicon glyphicon-cog" aria-hidden="true"></span> Proprocessing with <strong>Sass</strong>. Positioned <strong>in header</strong>. <a href='#change-settings-modal' role="button" data-toggle="modal" data-backdrop="true" aria-expanded="false" aria-controls="change-preprocessor-modal">Change Settings</a></p>
+					  </div>
+
+					</div>
+					<div class="clearfix" style="background-color:#f5f5f5;padding:10px;" >
+					  <button type="button" class="btn btn-primary pull-right"><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Save</button>
+						<p class="pull-right text-success" ><span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span> Saved</p>
+					</div>
+
 	    </div>
 	    <div role="tabpanel" class="tab-pane" id="javascript-edit">
 	    	
